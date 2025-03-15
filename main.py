51cd5cb72345bdd75369f21c9cb9b41d581cfe5d
maborjan173 <ingmbn125@gmail.com>
2025-03-15 01:05:43 -0500

    Corrección del main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from routes.user_routes import router as user_router
 from routes.ai_routes import router as ai_router
@@ -10,3 +12,8 @@ app.include_router(ai_router)
 @app.get("/")
 def read_root():
     return {"message": "API de ML funcionando correctamente"}
+
+# Esto asegura que Render use el puerto correcto
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Usa el puerto de Render o 8000 por defecto
+    uvicorn.run(app, host="0.0.0.0", port=port)
